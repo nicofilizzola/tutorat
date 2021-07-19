d800b3ab5ba9861916dee8495b7fe8b096afb983
nicofilizzola <nicofilizzola.prod@gmail.com>
2021-07-19 16:42:14 +0200

    add firstname and lastname properties to user entity

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,21 +4,39 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', null, [
+                'label' => 'Adresse mail de l\'IUT'
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => "Accepter conditions d'utilisation",
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('role', ChoiceType::class, [
+                'label' => "Je suis...",
+                'choices' => [
+                        'Étudiant' => 1,
+                        'Étudiant tuteur' => 2,
+                        'Administrateur' => 3
+                ],
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
@@ -29,6 +47,7 @@ class RegistrationFormType extends AbstractType
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'label' => 'Mot de passe',
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [

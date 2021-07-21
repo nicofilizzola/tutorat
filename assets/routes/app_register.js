abfa4e4c2408abc72b085786d0ef8568aebfef20
Alex <alex.gattefosse@gmail.com>
2021-07-21 12:23:04 +0200

    register page design
    choice between year and admin
    error design

--- assets/routes/app_register.js
+++ assets/routes/app_register.js
@@ -1,18 +1,29 @@
+import '../sass/utils/register.scss';
+
+const adminOrYearContainer = document.getElementById("adminOrYear--container");
+const yearInput = document.getElementById("registration_form_year");
+const adminCodeInput = document.getElementById("registration_form_adminCode");
+const roleInput = document.getElementById("registration_form_role");
+
 const manageAdminYearRole = () => {
   const manageDisabledInputs = (roleInputValue) => {
-    const yearInput = document.querySelector("#registration_form_year");
-    const adminCodeInput = document.querySelector("#registration_form_adminCode");
-
+    
     if (roleInputValue == 3) {
+      adminOrYearContainer.children[0].style.display = 'flex'
+      adminOrYearContainer.children[0].hidden = false
+      adminOrYearContainer.children[1].style.display = 'none'
+      adminOrYearContainer.children[1].hidden = true
       yearInput.disabled = true;
       adminCodeInput.disabled = false;
     } else {
+      adminOrYearContainer.children[0].style.display = 'none'
+      adminOrYearContainer.children[0].hidden = true
+      adminOrYearContainer.children[1].style.display = 'flex'
+      adminOrYearContainer.children[1].hidden = false
       yearInput.disabled = false;
       adminCodeInput.disabled = true;
     }
   };
-  
-  const roleInput = document.querySelector("#registration_form_role");
 
   manageDisabledInputs(roleInput.value); // if default value is admin when reloaded with error
   roleInput.addEventListener("change", function () {

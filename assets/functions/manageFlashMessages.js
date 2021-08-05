a42c555e5d01e089e5921a8901aea3246985c3b5
Alex <alex.gattefosse@gmail.com>
2021-08-05 15:34:38 +0200

    flash messages

---
+++ assets/functions/manageFlashMessages.js
@@ -0,0 +1,25 @@
+import { gsap } from 'gsap'
+
+function manageFlashMessages(message) {
+   const flashContainer = document.createElement('div')
+   flashContainer.classList.add('flash--container')
+
+   const flashContainerBackground = document.createElement('div')
+   flashContainerBackground.classList.add('background')
+
+   const textContainer = document.createElement('div')
+   textContainer.classList.add('text--container')
+
+   const cross = document.createElement('span')
+   cross.classList.add('cross')
+   cross.innerHTML = 'x'
+
+   textContainer.appendChild(message)
+   textContainer.appendChild(cross)
+   flashContainer.appendChild(textContainer)
+   flashContainer.appendChild(flashContainerBackground)
+
+   document.body.appendChild(flashContainer)
+}
+
+export { manageFlashMessages }

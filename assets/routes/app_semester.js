7c19690968fb43a13324f33c161ad783216e7efb
Alex <alex.gattefosse@gmail.com>
2021-08-16 15:27:26 +0200

    semesters + tutor hours in users page

---
+++ assets/routes/app_semester.js
@@ -0,0 +1,19 @@
+import "../sass/utils/form.scss";
+import "../sass/utils/users.scss";
+import "../sass/utils/semesters.scss";
+
+const deleteFormButtons = document.querySelectorAll('.delete')
+
+const getCard = (button) => {
+   return button.parentNode.parentNode
+}
+
+deleteFormButtons.forEach(button => {
+   button.addEventListener('mouseenter', () => {
+      getCard(button).style.backgroundColor = 'rgba(226, 226, 226, .5)'
+   })
+   
+   button.addEventListener('mouseleave', () => {
+      getCard(button).style.backgroundColor = ''
+   })
+})

37f5221999fb2f4f40b78121f12dad1184a78e03
Alex <alex.gattefosse@gmail.com>
2021-08-18 17:28:43 +0200

    sort functions

--- assets/routes/app_users.js
+++ assets/routes/app_users.js
@@ -5,6 +5,157 @@ const refuseFormButtons = document.querySelectorAll('.refuse')
 const deleteFormButtons = document.querySelectorAll('.delete')
 const filterButtons = document.querySelectorAll('.filter button')
 
+const usersListContainer = document.querySelector('.content--container')
+const usersList = document.querySelectorAll('.userContent--container__user')
+const tempUsersList = []
+
+usersList.forEach(user => {
+   tempUsersList.push(user)
+   user.remove()
+})
+
+sortByFirstname(false)
+// sortByLastname(true)
+// sortByTutorHours(false)
+// sortByMail(false)
+// sortByRole(false)
+// sortByStatut(true)
+
+
+function sortByTutorHours(isDesc) {
+   usersListContainer.children.forEach(user => {
+      if (user.classList.contains('userContent--container__user')) {
+         user.remove()
+      }
+   })
+
+   tempUsersList.sort(function (a, b) {
+      // return a.dataset.lastname.charAt(1).localeCompare(b.dataset.lastname.charAt(1), {ignorePunctuation: true})
+      return a.dataset.tutorhours.localeCompare(b.dataset.tutorhours)
+   })
+
+   if (isDesc) {
+      tempUsersList.reverse(function (a, b) {
+         // return b.dataset.lastname.charAt(1).localeCompare(a.dataset.lastname.charAt(1), {ignorePunctuation: true})
+         return b.dataset.tutorhours.localeCompare(a.dataset.tutorhours)
+      })
+   }
+
+   tempUsersList.forEach(user => {
+      usersListContainer.appendChild(user)
+   })
+}
+
+function sortByFirstname(isDesc) {
+   usersListContainer.children.forEach(user => {
+      if (user.classList.contains('userContent--container__user')) {
+         user.remove()
+      }
+   })
+
+   tempUsersList.sort(function (a, b) {
+      return a.dataset.firstname.charAt(1).localeCompare(b.dataset.firstname.charAt(1), {ignorePunctuation: true})
+   })
+
+   if (isDesc) {
+      tempUsersList.reverse(function (a, b) {
+         return a.dataset.firstname.charAt(1).localeCompare(b.dataset.firstname.charAt(1), {ignorePunctuation: true})
+      })
+   }
+
+   tempUsersList.forEach(user => {
+      usersListContainer.appendChild(user)
+   })
+}
+
+function sortByLastname(isDesc) {
+   usersListContainer.children.forEach(user => {
+      if (user.classList.contains('userContent--container__user')) {
+         user.remove()
+      }
+   })
+
+   tempUsersList.sort(function (a, b) {
+      return a.dataset.lastname.charAt(1).localeCompare(b.dataset.lastname.charAt(1), {ignorePunctuation: true})
+   })
+
+   if (isDesc) {
+      tempUsersList.reverse(function (a, b) {
+         return b.dataset.lastname.charAt(1).localeCompare(a.dataset.lastname.charAt(1), {ignorePunctuation: true})
+      })
+   }
+
+   tempUsersList.forEach(user => {
+      usersListContainer.appendChild(user)
+   })
+}
+
+function sortByMail(isDesc) {
+   usersListContainer.children.forEach(user => {
+      if (user.classList.contains('userContent--container__user')) {
+         user.remove()
+      }
+   })
+
+   tempUsersList.sort(function (a, b) {
+      return a.dataset.mail.charAt(1).localeCompare(b.dataset.mail.charAt(1), {ignorePunctuation: true})
+   })
+
+   if (isDesc) {
+      tempUsersList.reverse(function (a, b) {
+         return b.dataset.mail.charAt(1).localeCompare(a.dataset.mail.charAt(1), {ignorePunctuation: true})
+      })
+   }
+
+   tempUsersList.forEach(user => {
+      usersListContainer.appendChild(user)
+   })
+}
+
+function sortByRole(isDesc) {
+   usersListContainer.children.forEach(user => {
+      if (user.classList.contains('userContent--container__user')) {
+         user.remove()
+      }
+   })
+
+   tempUsersList.sort(function (a, b) {
+      return a.dataset.role.charAt(1).localeCompare(b.dataset.role.charAt(1), {ignorePunctuation: true})
+   })
+
+   if (isDesc) {
+      tempUsersList.reverse(function (a, b) {
+         return b.dataset.role.charAt(1).localeCompare(a.dataset.role.charAt(1), {ignorePunctuation: true})
+      })
+   }
+
+   tempUsersList.forEach(user => {
+      usersListContainer.appendChild(user)
+   })
+}
+
+function sortByStatut(isDesc) {
+   usersListContainer.children.forEach(user => {
+      if (user.classList.contains('userContent--container__user')) {
+         user.remove()
+      }
+   })
+
+   tempUsersList.sort(function (a, b) {
+      return a.dataset.statut - b.dataset.statut
+   })
+
+   if (isDesc) {
+      tempUsersList.reverse(function (a, b) {
+         return b.dataset.statut - a.dataset.statut
+      })
+   }
+
+   tempUsersList.forEach(user => {
+      usersListContainer.appendChild(user)
+   })
+}
+
 const getCard = (button) => {
    return button.parentNode.parentNode
 }

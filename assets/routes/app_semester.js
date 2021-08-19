c608ba4bb507c7f62053cc14b80644dbf00561ae
Alex <alex.gattefosse@gmail.com>
2021-08-19 14:49:18 +0200

    years check

--- assets/routes/app_semester.js
+++ assets/routes/app_semester.js
@@ -3,6 +3,7 @@ import "../sass/utils/users.scss";
 import "../sass/utils/semesters.scss";
 
 const deleteFormButtons = document.querySelectorAll('.delete')
+const yearSelects = document.querySelectorAll('select[data-year]')
 
 const getCard = (button) => {
    return button.parentNode.parentNode
@@ -16,4 +17,15 @@ deleteFormButtons.forEach(button => {
    button.addEventListener('mouseleave', () => {
       getCard(button).style.backgroundColor = ''
    })
+})
+
+yearSelects[0].addEventListener('change', () => {
+   for (let i = 0; i < yearSelects[1].children.length; i++) {
+      if (yearSelects[0].value >= yearSelects[1].children[i].value) {
+         yearSelects[1].children[i].disabled = true
+         yearSelects[1].selectedIndex = i+1
+      } else {
+         yearSelects[1].children[i].disabled = false
+      }
+   }
 })
